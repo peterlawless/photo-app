25bac0a4e928e4ba51d1a7ce456247ad426b1085
Peter Lawless <peterlawless223@gmail.com>
2017-05-31 14:43:59 -0400

    Misnamed keys in carrierwave config file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       :provider => 'AWS',
-      :aws_key_id => ENV['S3_ACCESS_KEY'],
-      :aws_secret_key => ENV['S3_SECRET_ACCESS_KEY'],
+      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_ACCESS_KEY'],
     }
     config.fog_directory = ENV['S3_BUCKET_NAME']
   end

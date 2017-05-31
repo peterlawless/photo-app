cdfd62559519ec5d6751901533d38d2371c55c70
Peter Lawless <peterlawless223@gmail.com>
2017-05-31 11:51:13 -0400

    Edited images views to display the actual image thumbnails, added resize_to_limit to PictureUploader

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,13 @@ class Image < ApplicationRecord
   belongs_to :user
   # associates the picture attribute on the Image model to the PictureUploader
   mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  private
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "Should be less than 5 MB")
+    end
+  end
+
 end

cb2977c494a5a4e2440b4542f0762afbdd6451b4
Peter Lawless <peterlawless223@gmail.com>
2017-05-31 14:42:24 -0400

    Error in carrierwave config file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws',
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       :provider => 'AWS',
       :aws_key_id => ENV['S3_ACCESS_KEY'],
       :aws_secret_key => ENV['S3_SECRET_ACCESS_KEY'],
-    },
+    }
     config.fog_directory = ENV['S3_BUCKET_NAME']
   end
 end
